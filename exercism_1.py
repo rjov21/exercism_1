2038966261b3608566d47ab190f4051c138446f4
Robinson Ordoñez <robinsonor4@gmail.com>
2021-04-05 17:30:59 -0500

    ejercicio 2

--- exercism_1.py
+++ exercism_1.py
@@ -13,3 +13,30 @@ def reverse(text="Ramen"):
     for i in text:
         a = i + a
     return a
+
+
+# high scores
+
+
+def latest(scores=[100, 0, 90, 30]):
+    expected = int(scores[len(scores) - 1])
+    return expected
+
+
+def personal_best(scores=[100, 0, 90, 30]):
+    scores.sort()
+    x = scores[(len(scores) - 1):]
+    mayor = x[0]
+    return mayor
+
+
+def personal_top_three(scores=[100, 0, 90, 30]):
+    scores.sort()
+    if len(scores) < 3:
+        x = scores[:]
+    else:
+        x = scores[(len(scores) - 3):]
+    mayor = []
+    for i in range(0, len(x)):
+        mayor.append(x[len(x) - i - 1])
+    return mayor
