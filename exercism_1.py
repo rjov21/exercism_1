c7c7127164659310297141ce474424c8325486ac
Robinson Ordoñez <robinsonor4@gmail.com>
2021-04-05 17:32:29 -0500

    ejercicio 3

--- exercism_1.py
+++ exercism_1.py
@@ -40,3 +40,38 @@ def personal_top_three(scores=[100, 0, 90, 30]):
     for i in range(0, len(x)):
         mayor.append(x[len(x) - i - 1])
     return mayor
+
+
+# scrabble score
+def score(word=""):
+    scorefinal = 0
+    for i in word:
+        if i == "a" or i == "e" or i == "i" or i == "o" or i == "u" or i == "l" or i == "n" or i == "r" or i == "t" or i == "s":
+            scorefinal = scorefinal + 1
+        if i == "d" or i == "g":
+            scorefinal = 2 + scorefinal
+        if i == "b" or i == "c" or i == "m" or i == "p":
+            scorefinal = scorefinal + 3
+        if i == "f" or i == "v" or i == "h" or i == "w" or i == "y":
+            scorefinal = scorefinal + 4
+        if i == "k":
+            scorefinal = scorefinal + 5
+        if i == "j" or i == "x":
+            scorefinal = scorefinal + 8
+        if i == "q" or i == "z":
+            scorefinal = scorefinal + 10
+        if i == "A" or i == "E" or i == "I" or i == "O" or i == "U" or i == "L" or i == "N" or i == "R" or i == "T" or i == "S":
+            scorefinal = scorefinal + 1
+        if i == "D" or i == "G":
+            scorefinal = 2 + scorefinal
+        if i == "B" or i == "C" or i == "M" or i == "P":
+            scorefinal = scorefinal + 3
+        if i == "F" or i == "V" or i == "H" or i == "W" or i == "Y":
+            scorefinal = scorefinal + 4
+        if i == "K":
+            scorefinal = scorefinal + 5
+        if i == "J" or i == "X":
+            scorefinal = scorefinal + 8
+        if i == "Q" or i == "Z":
+            scorefinal = scorefinal + 10
+    return scorefinal
